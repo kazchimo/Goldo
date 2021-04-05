127417d6da106f95f611d1b8569e682efd299a30
kazuki <pharcydetip@gmail.com>
2021-04-06 00:40:35 +0900

    feat: get sample list

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+    useEffect(() => {
+        window.gapi.client.setToken({access_token: window.localStorage.getItem("authToken")!})
+        window.gapi.client.request({
+            path: "https://tasks.googleapis.com/tasks/v1/users/@me/lists"
+        }).execute(r => console.log(r));
+    })
+
     return (
         <div className="App">
             {window.localStorage.getItem("authToken")}
