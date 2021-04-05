4671a2c085bd022d72291186d0db96ac239fdaf4
kazuki <pharcydetip@gmail.com>
2021-04-06 00:52:31 +0900

    style: fmt

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
 function App() {
-    useEffect(() => {
-        window.gapi.client.setToken({access_token: window.localStorage.getItem("authToken")!})
-        window.gapi.client.request({
-            path: "https://tasks.googleapis.com/tasks/v1/users/@me/lists"
-        }).execute(r => console.log(r));
-    })
+  useEffect(() => {
+    window.gapi.client.setToken({
+      access_token: window.localStorage.getItem("authToken")!,
+    });
 
-    return (
-        <div className="App">
-           hello
-        </div>
-    );
+    window.gapi.client
+      .request({
+        path: "https://tasks.googleapis.com/tasks/v1/users/@me/lists",
+      })
+      .execute((r) => console.log(r));
+  });
+
+  return <div className="App">hello</div>;
 }
 
 export default App;
