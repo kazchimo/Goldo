359d62aade9d22d164a4a950c099302ade558e92
kazuki <pharcydetip@gmail.com>
2021-04-06 00:25:55 +0900

    feat: set token and login if login succed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {window.localStorage.getItem("authToken")}
+            {window.localStorage.getItem("login")}
+            <button id="authorize_button">Authorize</button>
+            <button id="signout_button">Sign Out</button>
+        </div>
+    );
 }
 
 export default App;
