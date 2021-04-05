12a7ed617fedf24b2db71a17b65bb8592d4ccc2d
kazuki <pharcydetip@gmail.com>
2021-04-06 02:15:07 +0900

    feat: setup saga and reducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import App from "./App";
+import { reducer } from "./modules/reducers";
+import { allSagas } from "./modules/sagas";
+import reportWebVitals from "./reportWebVitals";
+import createSagaMiddleware from "redux-saga";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(allSagas);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
