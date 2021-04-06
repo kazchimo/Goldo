dd053c87700bcb36080fa427094bc460b4ae6cdb
kazuki <pharcydetip@gmail.com>
2021-04-06 12:20:10 +0900

    feat: add taskLists slice

--- src/modules/reducers.ts
+++ src/modules/reducers.ts
@@ -1,24 +1,32 @@
-import { createAction, createSlice } from "@reduxjs/toolkit";
+import { createAction, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { TaskList } from "../lib/gapi";
 
 export type State = {
   clientLoaded: boolean;
+  taskLists: TaskList[];
 };
 
 const initialState: State = {
   clientLoaded: false,
+  taskLists: [],
 };
 
 const slice = createSlice({
   name: "appSlice",
   reducers: {
     successLoadClient: (s) => ({ ...s, clientLoaded: true }),
+    successFetchTaskLists: (s, a: PayloadAction<TaskList[]>) => ({
+      ...s,
+      taskLists: a.payload,
+    }),
   },
   initialState,
 });
 
 const loadGapiClient = createAction("loadGapiClient");
+const fetchTaskLists = createAction("fetchTaskLists");
 
-const actions = { ...slice.actions, loadGapiClient };
+const actions = { ...slice.actions, loadGapiClient, fetchTaskLists };
 const reducer = slice.reducer;
 
 export { actions, reducer };
