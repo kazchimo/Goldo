0bc04c3248321b9dea7a4122253972b200a02df9
kazuki <pharcydetip@gmail.com>
2021-04-07 23:54:31 +0900

    chore: load auth2

--- src/modules/saga/gapiSaga.ts
+++ src/modules/saga/gapiSaga.ts
@@ -4,6 +4,7 @@ import { gapiActions } from "../slice/gapiSlice";
 function* loadGapi() {
   let clientFinish = false;
   let authFinish = false;
+  const auth2Finish = false;
 
   gapi.load("client", () => {
     clientFinish = true;
@@ -11,8 +12,11 @@ function* loadGapi() {
   gapi.load("auth", () => {
     authFinish = true;
   });
+  gapi.load("client:auth2", () => {
+    authFinish = true;
+  });
 
-  while (!clientFinish || !authFinish) {
+  while (!clientFinish || !authFinish || !auth2Finish) {
     yield delay(100);
   }
 
