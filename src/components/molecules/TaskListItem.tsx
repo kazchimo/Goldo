2b66f950589eb715f37a56fc68726a01e0399f21
kazuki <pharcydetip@gmail.com>
2021-04-11 02:52:30 +0900

    feat: impl nested task

--- src/components/molecules/TaskListItem.tsx
+++ src/components/molecules/TaskListItem.tsx
@@ -1,14 +1,16 @@
 import {
   Checkbox,
   Collapse,
+  List,
   ListItem,
   ListItemIcon,
   ListItemText,
+  makeStyles,
 } from "@material-ui/core";
-import React, { useState } from "react";
-import { Draggable } from "react-beautiful-dnd";
 import ExpandLess from "@material-ui/icons/ExpandLess";
 import ExpandMore from "@material-ui/icons/ExpandMore";
+import React, { useState } from "react";
+import { Draggable } from "react-beautiful-dnd";
 import { TaskView } from "../../modules/slice/taskSlice";
 
 type Props = {
@@ -16,9 +18,16 @@ type Props = {
   index: number;
 };
 
+const useStyles = makeStyles((theme) => ({
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+}));
+
 export const TaskListItem: React.FC<Props> = ({ task, index }) => {
   const [open, setOpen] = useState(false);
   const hasChildren = task.children.length > 0;
+  const classes = useStyles();
 
   return (
     <Draggable draggableId={"draggable-" + task.id} index={index}>
@@ -37,7 +46,13 @@ export const TaskListItem: React.FC<Props> = ({ task, index }) => {
             {hasChildren && (open ? <ExpandLess /> : <ExpandMore />)}
           </ListItem>
           {hasChildren && (
-            <Collapse in={open}>{task.children.map((t) => t.id)}</Collapse>
+            <Collapse in={open}>
+              <List dense className={classes.nested}>
+                {task.children.map((child, idx) => (
+                  <TaskListItem task={child} index={idx} key={child.id} />
+                ))}
+              </List>
+            </Collapse>
           )}
         </>
       )}
