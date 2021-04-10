a45d7544e3990fdc14528261bbf5c0898c0ac33b
kazuki <pharcydetip@gmail.com>
2021-04-11 02:33:37 +0900

    feat: impl nest list temp

--- src/components/molecules/TaskListItem.tsx
+++ src/components/molecules/TaskListItem.tsx
@@ -1,32 +1,45 @@
 import {
   Checkbox,
+  Collapse,
   ListItem,
   ListItemIcon,
   ListItemText,
 } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
 import { Draggable } from "react-beautiful-dnd";
-import { Task } from "../../lib/gapi";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import { TaskView } from "../../modules/slice/taskSlice";
 
 type Props = {
-  task: Task;
+  task: TaskView;
   index: number;
 };
 
 export const TaskListItem: React.FC<Props> = ({ task, index }) => {
+  const [open, setOpen] = useState(false);
+  const hasChildren = task.children.length > 0;
+
   return (
     <Draggable draggableId={"draggable-" + task.id} index={index}>
       {(provided) => (
-        <ListItem
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-        >
-          <ListItemIcon>
-            <Checkbox size={"small"} />
-          </ListItemIcon>
-          <ListItemText primary={task.title} />
-        </ListItem>
+        <>
+          <ListItem
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            onClick={hasChildren ? () => setOpen((a) => !a) : () => {}}
+          >
+            <ListItemIcon>
+              <Checkbox size={"small"} />
+            </ListItemIcon>
+            <ListItemText primary={task.title} />
+            {hasChildren && (open ? <ExpandLess /> : <ExpandMore />)}
+          </ListItem>
+          {hasChildren && (
+            <Collapse in={open}>{task.children.map((t) => t.id)}</Collapse>
+          )}
+        </>
       )}
     </Draggable>
   );
