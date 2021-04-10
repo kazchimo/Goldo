fb058bce3dbae4394bef2f843e3a5eb7fb78e169
kazuki <pharcydetip@gmail.com>
2021-04-10 21:08:05 +0900

    feat: only show not completed

--- src/lib/gapi.ts
+++ src/lib/gapi.ts
@@ -23,8 +23,12 @@ export const getClient = () => {
 export const getTaskLists = () =>
   gapi.client.tasks.tasklists.list({ maxResults: 100 });
 
-export const getTasks = (listId: string) =>
-  gapi.client.tasks.tasks.list({ tasklist: listId, maxResults: 100 });
+export const getTasks = (listId: string, showCompleted: boolean = false) =>
+  gapi.client.tasks.tasks.list({
+    tasklist: listId,
+    maxResults: 100,
+    showCompleted,
+  });
 
 export const calcExpiresAt = (expiresIn: string) =>
   Math.floor(new Date().getTime() / 1000) + Number(expiresIn);
