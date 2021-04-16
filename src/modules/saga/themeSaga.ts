6202967bcc72a579b664784c190569377a8393a5
kazuki <pharcydetip@gmail.com>
2021-04-16 13:01:06 +0900

    fix: warning

--- src/modules/saga/themeSaga.ts
+++ src/modules/saga/themeSaga.ts
@@ -1,4 +1,4 @@
-import { call, put, takeEvery, takeLatest } from "redux-saga/effects";
+import { put, takeEvery, takeLatest } from "redux-saga/effects";
 import { Action } from "typescript-fsa";
 import { themeActions } from "../slice/themeSlice";
 
