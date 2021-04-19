5e311c50483e187f14fee805fb49cfa2947ed033
kazuki <pharcydetip@gmail.com>
2021-04-19 22:41:58 +0900

    fix: warn

--- src/components/page/TaskListPage.tsx
+++ src/components/page/TaskListPage.tsx
@@ -1,4 +1,4 @@
-import { Divider, List, ListSubheader, Paper } from "@material-ui/core";
+import { List, ListSubheader, Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 import { useParams } from "react-router-dom";
