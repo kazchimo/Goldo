12b5cd3dfd0e201dd599ade883c250b540c5f277
kazuki <pharcydetip@gmail.com>
2021-04-25 23:21:29 +0900

    feat: add default list

--- src/components/page/SettingsPage.tsx
+++ src/components/page/SettingsPage.tsx
@@ -1,6 +1,44 @@
-import { Grid } from "@material-ui/core";
+import { Grid, Paper, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Form, Formik } from "formik";
 import React, { VFC } from "react";
+import { TaskListsSelect } from "../molecules/TaskListsSelect";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(3),
+  },
+  titleContainer: {
+    marginBottom: theme.spacing(2),
+  },
+}));
 
 export const SettingsPage: VFC = () => {
-  return <Grid>config</Grid>;
+  const classes = useStyles();
+
+  return (
+    <Grid container>
+      <Grid item xs={12} className={classes.titleContainer}>
+        <Typography>Settings</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>
+          <Formik initialValues={{ defaultListId: "" }} onSubmit={console.log}>
+            {() => (
+              <Grid container component={Form}>
+                <Grid item xs={12} container alignItems={"center"}>
+                  <Grid item xs={3}>
+                    <Typography>Default List</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TaskListsSelect />
+                  </Grid>
+                </Grid>
+              </Grid>
+            )}
+          </Formik>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
 };
