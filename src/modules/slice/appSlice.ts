62915abab6dc08197573fd7417921d160da0e104
kazuki <pharcydetip@gmail.com>
2021-05-04 03:05:18 +0900

    fix: remove unused import

--- src/modules/slice/appSlice.ts
+++ src/modules/slice/appSlice.ts
@@ -1,4 +1,4 @@
-import { createAction, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Task } from "../../lib/gapi";
 
 type State = {
